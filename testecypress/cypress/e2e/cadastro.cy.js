52e8863c22f20578740bb1598a2505c9196e4680
Leonardo <leofelix1983@gmail.com>
2026-09-02 11:12:36 -0300

    cadastro

--- testecypress/cypress/e2e/cadastro.cy.js
+++ testecypress/cypress/e2e/cadastro.cy.js
@@ -1,11 +1,11 @@
 import cadastroPage from "../support/pages/cadastro-page"
-
+import { faker } from '@faker-js/faker';
 describe('funcionalidade: cadastro', () => {
     beforeEach(() => {
         cy.visit("register.html")
     });
     it.only('deve fazer cadastro com  sucesso', () => {
-
-        cadastroPage.preencherCadastro("nardo","teste@teste.com","11922345678","teste123","teste123")
+       let email = faker.internet.email()
+        cadastroPage.preencherCadastro("nardo",email,"11922345678","teste123","teste123")
     });
 });
