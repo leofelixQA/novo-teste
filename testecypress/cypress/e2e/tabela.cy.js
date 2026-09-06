93e3b023bce82f9a94c811c6a1f5b3f17995c8a7
Leonardo <leofelix1983@gmail.com>
2026-09-06 14:55:33 -0300

    alteração

--- testecypress/cypress/e2e/tabela.cy.js
+++ testecypress/cypress/e2e/tabela.cy.js
@@ -7,19 +7,19 @@ describe('Funcionalidade: tabela', () => {
     it('deve pesquisar pelo livro 1984', () => {
         cy.get('#search-input').type("1984")
     });
-    
+
     it('deve buscar todos os livros do catalogo', () => {
-        cy.fixture("livros").then((catalog) =>{
-        catalog.forEach(book =>{
-             cy.get('#search-input').clear().type(book.livro)
-        })
+        cy.fixture("livros").then((catalog) => {
+            catalog.forEach(book => {
+                cy.get('#search-input').clear().type(book.livro)
+            })
         })
     });
 
-    it.only('deve fazer a busca por categoria', () => {
-        cy.fixture("livros").then((catalog2) =>{
-            catalog2.forEach(item =>{
-                 cy.get('#search-input').clear().type(item.livro)
+    it('deve fazer a busca por categoria', () => {
+        cy.fixture("livros").then((catalog2) => {
+            catalog2.forEach(item => {
+                cy.get('#search-input').clear().type(item.livro)
             })
         })
     });
